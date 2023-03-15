b5b466092c15a22cf21ec5687cec918b075a94dd
FullMetal-Dev <madinina972represent@gmail.com>
2023-03-15 22:29:15 +0100

    add footer

--- Views/default.php
+++ Views/default.php
@@ -16,8 +16,8 @@
         <div class="nav-wrapper blue darken-3">
             <a href="/" class="brand-logo ">MON BLOG</a>
             <ul id="nav-mobile" class="right hide-on-med-and-down">
-                
-                <?php if(isset($_SESSION['user']) && !empty($_SESSION['user']['id'])): ?>
+
+                <?php if (isset($_SESSION['user']) && !empty($_SESSION['user']['id'])) : ?>
                     <li><a href="/">Acceuil</a></li>
                     <li><a href="/post">Mes Publications</a></li>
                     <div class="chip">
@@ -25,17 +25,40 @@
                         Jane Doe
                     </div>
                     <li><a href="user/logout">Se déconnecter </a></li>
-                <?php else: ?>
+                <?php else : ?>
 
                     <li><a href="user/register">S'inscrire</a></li>
                     <li><a href="user/login">Se connecter</a></li>
 
-                <?php endif; ?>  
+                <?php endif; ?>
             </ul>
         </div>
     </nav>
 
     <div class="container"> <?= $content ?></div>
+
+    <footer class="page-footer blue darken-3">
+        <div class="container">
+            <div class="row">
+                <div class="col l6 s12">
+                    <h5 class="white-text">Mon Blog</h5>
+                    <p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+                </div>
+                <div class="col l4 offset-l2 s12">
+                    <h5 class="white-text">Links</h5>
+                    <ul>
+                        <li><a class="grey-text text-lighten-3" href="#!">Linkedin</a></li>
+                        <li><a class="grey-text text-lighten-3" href="#!">Github</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="footer-copyright">
+            <div class="container">
+                © 2023 Copyright Text
+            </div>
+        </div>
+    </footer>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 </body>
